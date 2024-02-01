38714adabc18b6598588eb6ecf09e21df5bcf3a2
AbudovaNubar <tu7XHU360@code.edu.az>
2024-02-01 17:42:38 +0400

    src and Presentation folder created

---
+++ EnchantElegance/Presentation/EnchantElegance/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EnchantElegance.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
