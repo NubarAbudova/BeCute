2eeed0e8f452b82de340e637e8a77c9842490345
AbudovaNubar <tu7XHU360@code.edu.az>
2024-02-01 18:19:17 +0400

    Front to Back

---
+++ EnchantElegance/src/Core/EnchantElegance.Domain/Entities/Common/BaseEntity.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnchantElegance.Domain.Entities.Common
+{
+    public abstract class BaseEntity
+    {
+        public int Id { get; set; }
+        public bool IsDeleted { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+        public string CreatedBy { get; set; } = null!;
+        public BaseEntity()
+        {
+            CreatedAt = DateTime.Now;
+            CreatedBy = "nubar.abudova";
+        }
+
+    }
+}
