7cfaf1b65e319732f508321c313f7348afa62330
AbudovaNubar <tu7XHU360@code.edu.az>
2024-02-03 01:45:30 +0400

    Category CRUD/Update

--- EnchantElegance/Presentation/EnchantElegance/Areas/Manage/Controllers/CategoryController.cs
+++ EnchantElegance/Presentation/EnchantElegance/Areas/Manage/Controllers/CategoryController.cs
@@ -20,13 +20,10 @@ namespace EnchantElegance.Areas.Manage.Controllers
             List<Category> categories = await _context.Categories.ToListAsync();
             return View(categories);
         }
-        public async Task<IActionResult> Create(CategoryCreateDTO categorydto)
+        public async Task<IActionResult> Create(CategoryCreateDTO createDTO)
         {
-			if (!ModelState.IsValid)
-			{
-				return View();
-			}
-			bool result = await _context.Categories.AnyAsync(c => c.Name.Trim() == categorydto.Name.Trim());
+			if (!ModelState.IsValid) return View();
+			bool result = await _context.Categories.AnyAsync(c => c.Name.Trim() == createDTO.Name.Trim());
 			if (result)
 			{
 				ModelState.AddModelError("Name", "Category name already exists");
@@ -34,12 +31,48 @@ namespace EnchantElegance.Areas.Manage.Controllers
 			}
 			Category category = new Category
 			{
-				Name = categorydto.Name,
-				Description = categorydto.Description,
+				Name = createDTO.Name,
+				Description = createDTO.Description,
 			};
 			await _context.Categories.AddAsync(category);
 			await _context.SaveChangesAsync();
 			return RedirectToAction(nameof(Index));
         }
-    }
+		public async Task<IActionResult> Update(int id)
+		{
+			if (id <= 0) return BadRequest();
+			Category category = await _context.Categories.FirstOrDefaultAsync(c => c.Id == id);
+			if (category == null) return NotFound();
+			CategoryUpdateDTO updateDTO = new CategoryUpdateDTO(category.Id, category.Name, category.Description);
+			return View(updateDTO);
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> Update(int id, CategoryUpdateDTO updateDTO)
+		{
+			if (!ModelState.IsValid) return View();
+
+			Category existed = await _context.Categories.FirstOrDefaultAsync(c => c.Id == id);
+
+			if (existed == null) return NotFound();
+
+			if (existed.Name != updateDTO.Name && !string.IsNullOrEmpty(updateDTO.Name))
+			{
+				bool result = await _context.Categories.AnyAsync(c => c.Name == updateDTO.Name);
+
+				if (result)
+				{
+					ModelState.AddModelError("Name", "Category name already exists");
+					return View();
+				}
+			}
+
+			existed.Name = updateDTO.Name;
+			existed.Description = updateDTO.Description;
+
+			await _context.SaveChangesAsync();
+			return RedirectToAction(nameof(Index));
+		}
+
+	}
 }
